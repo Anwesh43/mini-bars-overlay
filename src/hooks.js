9a73e1143dd8816b37806bad96574b7a8ca61079
Anwesh43 <anweshthecool0@gmail.com>
2020-07-22 17:09:08 +0530

    adding useStyle hook

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,5 @@
 import {useState, useEffect} from 'react'
+import {sinify, divideScale} from './utils'
 
 export const useAnimatedScale = (scGap = 0.02, delay = 20) => {
   const [scale, setScale] = useState(0)
@@ -37,4 +38,32 @@ export const useDimension = () => {
       }
     }
   })
+}
+
+export const useStyle = (w, h, scale, n) => {
+  const gap = w / n 
+  const position = 'absolute'
+  const fixedBackground = 'green'
+  const dynamicBackground = 'indigo'
+  const fixedHeight = `${gap}px`
+  return {
+    getFixedBlockStyle() {
+      const width = `${w}px`
+      const height = fixedHeight 
+      const background = fixedBackground 
+      const top = `${0}px`
+      const left = `${0}px`
+      return {position, left, top, width, height, background};
+    },
+  
+    getDynamicBlockStyle(i) {
+      const sfi = sinify(divideScale(scale, i, n))
+      const height = `${gap * sfi}px`
+      const top = `${0}px`
+      const left = `${gap * i}px`
+      const width = `${gap}px`
+      const background = dynamicBackground 
+      return {position, left, top, width, height, background}
+    }
+  }
 }
