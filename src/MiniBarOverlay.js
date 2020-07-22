d74158e2080cef03e5a5fcacc0c39cee6506f97f
Anwesh43 <anweshthecool0@gmail.com>
2020-07-22 17:21:38 +0530

    adding MiniBarOverlay

---
+++ src/MiniBarOverlay.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {useStyle} from './hooks'
+
+const Block = ({style}) => {
+  return <div style = {style}>
+    </div>
+}
+
+const getDynamicBlocks = (i, style) => {
+  const blocks = []
+  for (let i = 0; i < n; i++) {
+    blocks.push(<Block key = {`dynamic_block_${i}`} style = {style(i)}/>)
+  }
+  return blocks 
+}
+
+const MiniBarOverlay = ({w, h, scale, n, onClick}) => {
+  const {getFixedBlockStyle, getDynamicBlockStyle} = useStyle(w, h, scale, n)
+  return <div onClick = {onClick}>
+      <Block style = {getFixedBlockStyle()}/>
+      {getDynamicBlocks(i, getDynamicBlockStyle)}
+    </div>
+}
+
+export default MiniBarOverlay
